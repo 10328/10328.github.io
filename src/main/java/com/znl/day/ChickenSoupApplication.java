dd2a7bfa86c843e960111bef6242e35cc81aeb37
zach-pro； <onesuperman2019@outlook.com>
2022-12-20 16:46:34 +0800

    commit

---
+++ src/main/java/com/znl/day/ChickenSoupApplication.java
@@ -0,0 +1,16 @@
+package com.znl.day;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zach
+ */
+@SpringBootApplication
+public class ChickenSoupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChickenSoupApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/znl/day/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.znl.day.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author dell
+ * @描述:
+ * @文件名: HelloController.java
+ * @创建人: zyhNick
+ * @创建时间: 13:40 2021/3/11
+ * @修改人:
+ * @修改备注: Copyright  2021/3/11
+ */
+@Controller
+public class UserController {
+
+    @RequestMapping("")
+    public String login(Model model) {
+        model.addAttribute("good_good_study", "假期定了个Plan，半个暑假结束了只完成了P，因为lan。");
+        return "html/dayLogin.html";
+    }
+
+    @RequestMapping("/books")
+    public String books(Model model) {
+        return "html/books.html";
+    }
+}
